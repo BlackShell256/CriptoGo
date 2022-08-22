e5aeabec91c451e2dc7f00c197d6a64bfd1d5566
ßłąck$hełł <104540054+BlackShell256@users.noreply.github.com>
2022-08-22 00:18:59 -0400

    Update CriptoGo.go

--- CriptoGo.go
+++ CriptoGo.go
@@ -1,98 +1,121 @@
 package main
 
 import (
-  "strings"
-  "fmt"
-  "log"
-  "io/ioutil"
-  "os"
-  "path/filepath"
-  "github.com/mkmueller/aes256"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/mkmueller/aes256"
 )
 
-func main(){
- Dec("hola") 
-  
+func main() {
+	color.Blue("Escoge lo que necesites\n\n")
+	color.Red(" [1] Encriptar")
+	color.Red(" [2] Desencriptar\n\n")
+
+	fmt.Print(color.RedString("> "))
+	buf := bufio.NewScanner(os.Stdin)
+	buf.Scan()
+	op := buf.Text()
+
+	color.Blue("\nIngresa la contraseña de encriptado o desencriptado\n\n")
+
+	fmt.Print(color.RedString("> "))
+	buf2 := bufio.NewScanner(os.Stdin)
+	buf2.Scan()
+	pass := buf2.Text()
+
+	switch op {
+	case "1":
+		Enc(pass)
+	case "2":
+		Dec(pass)
+	default:
+		color.Red(" [X] Opcion incorrecta")
+		time.Sleep(2 * time.Second)
+		main()
+	}
+
+	color.Blue("\n\n [*] Finalizado")
 }
 
-
-func GetFileEx() (dir string)  {
+func GetFileEx() (dir string) {
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
 
-	exPath := filepath.Dir(ex)
-	Env := os.Getenv("TEMP")
-	end := strings.HasPrefix(ex, Env)
+	end := strings.HasPrefix(ex, os.Getenv("TEMP"))
 
 	if end {
-		temp, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
-		dir = temp
+		dir = filepath.Base(strings.TrimSuffix(os.Args[0], ".exe") + ".go")
 	} else {
-		dir = exPath
+		dir = ex
 	}
 
-	return 
-} 
+	return
+}
 
-func GetFiles()  (List [] string) {
-	Ex := GetFileEx() 
+func GetFiles() (List []string) {
+	Ex := GetFileEx()
 	files, err := ioutil.ReadDir(".")
-    if err != nil {
-        log.Fatal(err)
-    }
- 
-    for _, f := range files {
-            if f.Name() == Ex {
-            	continue
-            } else if f.IsDir() {
-	  	 continue
-	 }
-            List = append(List,  f.Name()) 
-    }
-	
-    return
-} 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.Name() == Ex {
+			continue
+		} else if f.IsDir() {
+			continue
+		}
+		List = append(List, f.Name())
+	}
+
+	return
+}
 
 func Enc(Key string) {
-	
-	for _,  File := range GetFiles()  {
-   content, _ := ioutil.ReadFile(File)
 
-  f, err := os.OpenFile(File, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-  if err != nil {
-    log.Fatal(err)
-  }
-  Out,  _ := aes256.Encrypt(Key, content)  
-  fmt.Fprintf(f, "%d",  Out)
-  err = f.Close()
-  if err != nil {
-      log.Fatal(err) 
-  }  
-  
- } 
+	for _, File := range GetFiles() {
+		content, _ := os.ReadFile(File)
+		Out, _ := aes256.Encrypt(Key, content)
+		err := OpenFile(File, Out)
+		if err != nil {
+			continue
+		}
+
+	}
 }
 
-func Dec(Key string) { 	
-	for _,  File := range GetFiles()  {
-   content, _ := ioutil.ReadFile(File)
-  f, err := os.OpenFile(File, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-  if err != nil {
-    log.Fatal(err)
-  }
-  Out,  _ := aes256.Decrypt(Key, content)  
-  fmt.Fprintf(f, "%v",  Out)
-  err = f.Close()
-  if err != nil {
-      log.Fatal(err) 
-  }  
-  
- } 
+func OpenFile(name string, content []byte) (err error) {
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return
+	}
+
+	f.Write(content)
+	err = f.Close()
+	if err != nil {
+		return
+	}
+	return
 }
 
+func Dec(Key string) {
+	for _, File := range GetFiles() {
+		content, _ := os.ReadFile(File)
+		Out, _ := aes256.Decrypt(Key, content)
+		err := OpenFile(File, Out)
+		if err != nil {
+			continue
+		}
 
-
+	}
+}
